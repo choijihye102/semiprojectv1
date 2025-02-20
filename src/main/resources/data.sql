ce9a26e92bd4798eaa578164f0c3e54fcde25600
ChoiJiHye <stdy1227@gmail.com>
2025-02-20 12:39:40 +0900

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- members 테이블
+insert into members (userid, passwd, name, email)
+values
+    ('abc123', 'abc123','abc123','abc123@abc123.kr'),
+    ('987xyz', '987xyz','987xyz','987xyz@abc123.kr');
+
+
